9c77b838f31e75c10515d29e1eb3680aa2e6eac2
Aleksey Zuiev <aleksei.zuew@gmail.com>
2017-06-29 01:29:51 +0300

    iOSApp -> initial commit

---
+++ iOSApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSApp
+//
+//  Created by Aleksey Zuiev on 29/06/2017.
+//  Copyright © 2017 Aleksey Zuiev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
