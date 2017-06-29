b6f912558a7bae596ff623f58ec9185ccb17974b
Aleksey Zuiev <aleksei.zuew@gmail.com>
2017-06-30 01:05:50 +0300

    project -> recreate, restructure kiwi -> install pods

---
+++ Source/AZAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AZAppDelegate.h
+//  iOSApp
+//
+//  Created by Aleksey Zuiev on 30/06/2017.
+//  Copyright © 2017 Aleksey Zuiev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Source/AZViewController.h
@@ -0,0 +1,13 @@
+//
+//  AZViewController.h
+//  iOSApp
+//
+//  Created by Aleksey Zuiev on 30/06/2017.
+//  Copyright © 2017 Aleksey Zuiev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AZViewController : UIViewController
+
+@end
