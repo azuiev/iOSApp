540fafcc149d8d8e4f90e7cc229a7f11d4dbe120
Aleksey Zuiev <aleksei.zuew@gmail.com>
2017-06-30 01:06:19 +0300

    add prefix

--- iOSApp/main.m
+++ iOSApp/main.m
@@ -2,15 +2,15 @@
 //  main.m
 //  iOSApp
 //
-//  Created by Aleksey Zuiev on 29/06/2017.
+//  Created by Aleksey Zuiev on 30/06/2017.
 //  Copyright © 2017 Aleksey Zuiev. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
-#import "AppDelegate.h"
+#import "AZAppDelegate.h"
 
 int main(int argc, char * argv[]) {
     @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AZAppDelegate class]));
     }
 }
