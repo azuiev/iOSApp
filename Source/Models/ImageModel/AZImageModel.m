f55cae97cac5d7510d7515492f90dcef08672895
Aleksey Zuiev <aleksei.zuew@gmail.com>
2017-08-18 22:43:52 +0300

    refactor PerformLoading in AZImageModel

--- Source/Models/ImageModel/AZImageModel.m
+++ Source/Models/ImageModel/AZImageModel.m
@@ -11,14 +11,18 @@
 #import "AZGCD.h"
 #import "AZMacros.h"
 
-static NSString   *kImageURL        = @"kImageURL";
+static NSString     *kImageURL          = @"kImageURL";
+static double       AZLoadImageDelay    = 1.0;
+static NSString     *AZImageDirectory   = @"Images";
 
 @interface AZImageModel ()
 @property (nonatomic, strong) UIImage   *image;
 @property (nonatomic, strong) NSURL     *url;
+@property (nonatomic, strong) NSString  *casheName;
 
 - (NSString *)pathToImages;
-- (NSString *)nameOfCasheFileWithURL:(NSURL *)url;
+- (NSString *)nameOfCashedFile;
+- (UIImage *)imageFromCashe;
 
 @end
 
@@ -34,30 +38,22 @@ static NSString   *kImageURL        = @"kImageURL";
 }
 
 #pragma mark -
+#pragma mark Accessors 
+
+- (void)setUrl:(NSURL *)url {
+    if (_url != url) {
+        _url = url;
+        
+        self.casheName = [self nameOfCashedFile];
+    }
+}
+
+#pragma mark -
 #pragma mark LoadingModel
 
 - (void)performLoading {
-    [AZGCD dispatchAfterDelay:1.0 block:^ {
-        UIImage *image = nil;
-        NSString *cashedFileName = [self nameOfCasheFileWithURL:self.url];
-        
-        if ([[NSFileManager defaultManager] fileExistsAtPath:cashedFileName]) {
-            image = [UIImage imageNamed:cashedFileName];
-        } else {
-            NSData *imageData = [NSData dataWithContentsOfURL:self.url];
-            [[NSFileManager defaultManager] createDirectoryAtPath:[self pathToImages]
-                                      withIntermediateDirectories:YES
-                                                       attributes:nil
-                                                            error:nil];
-            BOOL saved = [imageData writeToFile:cashedFileName atomically:YES];
-            if (!saved) {
-                NSLog(@"Achtung!!!");
-            }
-            
-            image = [UIImage imageWithData:imageData];
-        }
-        
-        
+    [AZGCD dispatchAfterDelay:AZLoadImageDelay block:^ {
+        UIImage *image = [self loadImage];
         self.image = image;
         
         [AZGCD dispatchAsyncOnMainQueue: ^ {
@@ -69,17 +65,70 @@ static NSString   *kImageURL        = @"kImageURL";
 #pragma mark -
 #pragma mark Private Methods
 
-- (NSString *)pathToImages {
-    NSArray *paths = NSSearchPathForDirectoriesInDomains (NSDocumentDirectory, NSUserDomainMask, YES);
-    return [[paths firstObject] stringByAppendingPathComponent:@"Images"];
+- (UIImage *)loadImage {
+    return [self isCasheExist] ? [self imageFromCashe] : [self imageFromURL];
 }
 
-- (NSString *)nameOfCasheFileWithURL:(NSURL *)url {
+- (BOOL)isCasheExist {
+    return [NSFileManager.defaultManager fileExistsAtPath:self.casheName];
+}
+
+- (NSString *)pathToImages {
+    NSArray *paths = NSSearchPathForDirectoriesInDomains (NSDocumentDirectory, NSUserDomainMask, YES);
+    
+    return [[paths firstObject] stringByAppendingPathComponent:AZImageDirectory];
+}
+
+- (NSString *)nameOfCashedFile {
+    NSURL *url = self.url;
+    
     NSString *result = [self pathToImages];
+    
     result = [result stringByAppendingPathComponent:[NSString stringWithFormat:@"%lu", url.absoluteString.hash]];
     result = [result stringByAppendingString:url.lastPathComponent];
     
     return result;
+}
+
+- (UIImage *)imageFromCashe {
+    NSString *casheName = self.casheName;
+    UIImage *image = [UIImage imageNamed:casheName];
+   
+    if (!image) {
+        NSError *error = nil;
+        [NSFileManager.defaultManager removeItemAtPath:casheName error:&error];
+        image = [self imageFromURL];
+    }
+    
+    return image;
+}
+
+- (UIImage *)imageFromURL {
+    NSData *imageData = [NSData dataWithContentsOfURL:self.url];
+    
+    [self createCasheFile:imageData];
+    
+    return [UIImage imageWithData:imageData];
+}
+
+- (void)createCasheFile:(NSData *)imageData {
+    NSString *path = [self pathToImages];
+    NSFileManager *manager = NSFileManager.defaultManager;
+    NSError *error = nil;
+    
+    if (![manager fileExistsAtPath:path]) {
+        [manager createDirectoryAtPath:[self pathToImages]
+           withIntermediateDirectories:YES
+                            attributes:nil
+                                 error:&error];
+    }
+    
+    if (!error) {
+        BOOL saved = [imageData writeToFile:[self nameOfCashedFile] atomically:YES];
+        if (!saved) {
+            NSLog(@"Unable to save cashed image");
+        }
+    }
 }
 
 #pragma mark -
